e55843b5bf0a0d0eb4579314285d113a5d008640
TallestLegacy <tallestlegacy@gmail.com>
2022-03-29 12:02:06 +0300

    asnoyther passhref

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,26 +1,26 @@
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
+import styles from "../styles/Home.module.css"
+import Link from "next/link"
 
 const Home = () => {
-  return (
-    <>
-      <header className={styles.header}>
-        <span className={styles.heading}>Greeen Shop</span>
-      </header>
+	return (
+		<>
+			<header className={styles.header}>
+				<span className={styles.heading}>Greeen Shop</span>
+			</header>
 
-      <main className={styles.main}>
-        <div className="description">
-          Green shop is an e-commerce platforms for farmers across the country,
-          used to establish a market for everyone and everything.
-        </div>
-        <div>
-          <Link href="/test">
-            <button className={styles.button}>Test the API</button>
-          </Link>
-        </div>
-      </main>
-    </>
-  );
-};
+			<main className={styles.main}>
+				<div className="description">
+					Green shop is an e-commerce platforms for farmers across the country,
+					used to establish a market for everyone and everything.
+				</div>
+				<div>
+					<Link href="/test" passHref={true}>
+						<button className={styles.button}>Test the API</button>
+					</Link>
+				</div>
+			</main>
+		</>
+	)
+}
 
-export default Home;
+export default Home
